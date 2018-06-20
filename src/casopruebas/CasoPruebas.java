1c7f5b016533b31aca8244007bfa58e78dea64f3
ch-edison <edisonchavez14@gmail.com>
2018-06-20 06:26:04 -0500

    test application

---
+++ src/casopruebas/CasoPruebas.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casopruebas;
+
+/**
+ *
+ * @author USUARIO
+ */
+abstract class CasoPruebas {
+
+    
+}
